ba1319a3a0558eb3caefd00aa3cc52a928ec3ca4
Jian Wang <jianopt@ad.unc.edu>
2018-09-12 11:55:49 -0400

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="naspy",
-    version="0.0.1",
+    version="0.0.2",
     author="Jian Wang",
     author_email="jianopt@gmail.com",
     description="Nucleic acids Structure Prediction",
